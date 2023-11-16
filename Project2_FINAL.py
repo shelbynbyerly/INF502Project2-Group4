a41956f384a4713db32adb344917792a5f463bad
shelbynbyerly <146392348+shelbynbyerly@users.noreply.github.com>
2023-11-16 12:09:13 -0700

    Update Project2_FINAL.py

--- Project2_FINAL.py
+++ Project2_FINAL.py
@@ -1,3 +1,15 @@
 # API and Web Scraping 
 # Authors: 
 # Description: 
+
+# Menu 
+
+# Use GitHub API to collect information about GitHub repositories 
+
+# Relate each repository to a list of pull requests
+
+# Scape information from user profile page 
+
+# save_as_csv function 
+
+# Unit tests
